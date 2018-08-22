1544098bbb7ca265c3540ce0532c1927e22600a4
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2018-08-22 21:19:53 +0200

    Remove commented out code

--- download_data.py
+++ download_data.py
@@ -3,12 +3,6 @@ import json
 import pocket
 from pocket import Pocket
 
-# uri = 'https://example.com'
-# token = Pocket.get_request_token(consumer_key=key, redirect_uri=uri)
-# auth_url = Pocket.get_auth_url(code=token, redirect_uri=uri)
-# user_credentials = Pocket.get_credentials(consumer_key=key, code=token)
-# access_token = user_credentials['access_token']
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 consumer_key = config['POCKET']['CONSUMER_KEY']
